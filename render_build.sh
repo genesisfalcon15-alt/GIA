39f91f8d48b051e64d12cb915208e95fcd07d432
Génesis Falcón <genesisfalcon15@gmail.com>
2026-08-19 17:24:36 +0200

    fix: limpiar alembic_version antes de migrar en render

--- render_build.sh
+++ render_build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install pipenv
@@ -8,4 +7,16 @@ npm run build
 
 pipenv install
 
-pipenv run upgrade
+cd src && pipenv run python -c "
+from app import app
+from api.models import db
+with app.app_context():
+    try:
+        db.session.execute(db.text('DELETE FROM alembic_version'))
+        db.session.commit()
+        print('alembic_version limpiada')
+    except Exception as e:
+        print(f'no se pudo limpiar: {e}')
+" && cd ..
+
+cd src && pipenv run flask --app app db upgrade && cd ..
