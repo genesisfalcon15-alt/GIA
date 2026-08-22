e46cb5e0f4c32fb9e0f7e67b597e40b0c0cd3ec0
Génesis Falcón <genesisfalcon15@gmail.com>
2026-08-23 00:29:51 +0200

    fix: merge alembic heads para produccion

---
+++ src/migrations/versions/c1a392e4837d_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""merge multiple heads
+
+Revision ID: c1a392e4837d
+Revises: 7092778c2524
+Create Date: 2026-08-23 00:26:13.811274
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c1a392e4837d'
+down_revision = '7092778c2524'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
