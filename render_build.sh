f695fa88f13894d7875f8f22e722c20dbe70307a
Génesis Falcón <genesisfalcon15@gmail.com>
2026-08-22 21:47:47 +0200

    Fix: agregar columna category a tabla project en produccion

--- render_build.sh
+++ render_build.sh
@@ -7,5 +7,7 @@ npm run build
 
 pipenv install
 
+# Agrega la columna 'category' directamente en PostgreSQL si no existe
+cd src && pipenv run python -c "from app import app, db; db.session.execute(db.text('ALTER TABLE project ADD COLUMN IF NOT EXISTS category VARCHAR(100);')); db.session.commit()" && cd ..
+
 cd src && pipenv run flask --app app db stamp heads && cd ..
-cd src && pipenv run flask --app app db upgrade heads && cd ..
