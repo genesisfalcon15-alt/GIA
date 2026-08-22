e0b8c9a2344e36ac6fa28fcf9514cb998242944b
Génesis Falcón <genesisfalcon15@gmail.com>
2026-08-22 19:43:19 +0200

    Fix: stamp heads en lugar de borrar alembic

--- render_build.sh
+++ render_build.sh
@@ -7,16 +7,5 @@ npm run build
 
 pipenv install
 
-cd src && pipenv run python -c "
-from app import app
-from api.models import db
-with app.app_context():
-    try:
-        db.session.execute(db.text('DELETE FROM alembic_version'))
-        db.session.commit()
-        print('alembic_version limpiada')
-    except Exception as e:
-        print(f'no se pudo limpiar: {e}')
-" && cd ..
-
+cd src && pipenv run flask --app app db stamp heads && cd ..
 cd src && pipenv run flask --app app db upgrade heads && cd ..
